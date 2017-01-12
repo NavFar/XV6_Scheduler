7e1ba5414b4059eb420ec65321a3c485e230c939
NavidFarahmand <NavidFarahmand75@gmail.com>
2017-01-12 22:07:26 +0330

    add field to proc.h

--- sysproc.c
+++ sysproc.c
@@ -99,7 +99,7 @@ sys_getPerformanceData(void) {
     char *wtime = 0, *rtime = 0;
     argptr(0, &wtime, sizeof(int));
     argptr(1, &rtime, sizeof(int));
-    // *wtime = (proc->etime - proc->ctime) - proc->rtime;
-    // *rtime = proc->rtime;
+    *wtime = (proc->etime - proc->ctime) - proc->rtime;
+    *rtime = proc->rtime;
     return 0;
 }
